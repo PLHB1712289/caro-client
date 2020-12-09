a4101302b94ad1ff687659431ddd312fc4fe8e33
Vo Hoai An <vohoaian.tn@gmail.com>
2020-12-09 20:11:04 +0700

    Bat su kien khi co nguoi vao

--- src/component/page/index.js
+++ src/component/page/index.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route, useRouteMatch, useHistory } from "react-router-dom";
-
+import io from "socket.io-client";
+import config from "../../config";
 import Header from "../header";
 import NotFound from "../notFound";
+const socket = io(config.URL_SERVER, { transports: ["websocket"] });
 
 const Page = () => {
   // React router hook
   const match = useRouteMatch();
   const history = useHistory();
-
+  const [numOfOnlineUsers, setNumOfOnlineUsers] = useState(0);
+  socket.on("updateNumOfOnlineUsers", (data) => setNumOfOnlineUsers(data));
   // component didmount
   useEffect(() => {
     const token = localStorage.getItem("token");
-
     if (!token) {
       history.push("/sign-in");
       return;
@@ -26,9 +28,9 @@ const Page = () => {
         <Route path={`${match.url}profile`}>
           <div>Profile</div>
         </Route>
-
         <Route path={`${match.url}`} exact>
           <div>Home Page</div>
+          <div>Số lượng người đang online: {numOfOnlineUsers}</div>
         </Route>
 
         <Route>
