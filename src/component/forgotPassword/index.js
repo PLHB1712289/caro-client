39087d5f36b25973d5c0917873dd89d0edd5d5ea
PLHB1712289 <57471680+PLHB1712289@users.noreply.github.com>
2021-01-03 11:09:42 +0700

    fix loading

--- src/component/forgotPassword/index.js
+++ src/component/forgotPassword/index.js
@@ -1,122 +1,95 @@
-import {
-  Avatar,
-  Button,
-  Container,
-  CssBaseline,
-  TextField,
-  Typography,
-  Grid,
-  Link
-} from "@material-ui/core";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-
-import { useHistory } from "react-router-dom";
-import action from "../../storage/action";
-import useStyles from "./style";
+import { Button, Container, CssBaseline, TextField } from "@material-ui/core";
+import React, { useState } from "react";
 import apiService from "../profile/apiService";
+import useStyles from "./style";
 
 const ForgotPassword = () => {
   // Styles
   const classes = useStyles();
-  const history = useHistory();
 
   // States
-    
-  const [email,setEmail]=useState(null);
-  const [errorEmail,setErrorEmail]=useState(null);
+
+  const [email, setEmail] = useState(null);
+  const [errorEmail] = useState(null);
 
   //handle
-  const _handleChangeEmail=(e)=>{
+  const _handleChangeEmail = (e) => {
     const { value } = e.target;
     setEmail(value);
   };
-  const _handleSubmitForm=async(e)=>{
+  const _handleSubmitForm = async (e) => {
     e.preventDefault();
-    if(email!==null)
-    {
-      const {success,message,data}=await apiService.forgotPassword(email);
+    if (email !== null) {
+      const { message } = await apiService.forgotPassword(email);
       alert(message);
     }
-  }
-  return(
+  };
+  return (
     <div
       style={{
-      justifyContent:'center',
-      alignItems:'center',
-      display:'flex',
-      flexDirection:'column',
-    }}
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
     >
       <Container
-          component="main"
-          maxWidth="xs"
-          style={{
-              border: "1px solid white",
-              alignSelf: "center",
-              justifyContent: "center",
-              backgroundColor: "rgba(255,255,255,0.9)",
-              
-      }}
+        component="main"
+        maxWidth="xs"
+        style={{
+          border: "1px solid white",
+          alignSelf: "center",
+          justifyContent: "center",
+          backgroundColor: "rgba(255,255,255,0.9)",
+        }}
       >
-    <CssBaseline />
-    <div className={classes.paper}>
-    
-        <h1>Forget Password</h1>
-        <form
+        <CssBaseline />
+        <div className={classes.paper}>
+          <h1>Forget Password</h1>
+          <form
             className={classes.form}
             noValidate
             onSubmit={_handleSubmitForm}
-        >
-            
-            {errorEmail===null?
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  label="Email"
-                  autoFocus
-                  value={email}
-                  onChange={_handleChangeEmail}
-                />:
-                <TextField
-                  error
-                  autoFocus
-                  fullWidth
-                  variant="outlined"
-                  margin="normal"
-                  label="Email"
-                  value={email}
-                  onChange={_handleChangeEmail}
-                  helperText={errorEmail}
-                />
-            }
-            
-           
+          >
+            {errorEmail === null ? (
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Email"
+                autoFocus
+                value={email}
+                onChange={_handleChangeEmail}
+              />
+            ) : (
+              <TextField
+                error
+                autoFocus
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                label="Email"
+                value={email}
+                onChange={_handleChangeEmail}
+                helperText={errorEmail}
+              />
+            )}
+
             <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
             >
-                Send Mail
+              Send Mail
             </Button>
-        
-
-        
-        </form>
-        
+          </form>
+        </div>
+      </Container>
     </div>
-    </Container>
-
-   
-</div>
-
   );
-
 };
 
 export default ForgotPassword;
--- src/component/profile/index.js
+++ src/component/profile/index.js
@@ -1,273 +1,167 @@
-import {
-    Avatar,
-    Button,
-    Container,
-    CssBaseline,
-    TextField,
-    Typography,
-    Grid,
-    Link
-  } from "@material-ui/core";
-  import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-  import React, { useEffect, useState } from "react";
-  import { connect } from "react-redux";
-  import Chart from 'react-apexcharts'
+import { Container, CssBaseline, Grid, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import apiService from "./apiService";
+import useStyles from "./style";
+import action from "../../storage/action";
+import { connect } from "react-redux";
 
-  import { useHistory } from "react-router-dom";
-  import action from "../../storage/action";
-  import useStyles from "./style";
-  import apiService from "./apiService";
- 
-  const Profile = () => {
-    // Styles
-    const classes = useStyles();
-    const history = useHistory();
-  
-    // States
-    const [email, setEmail] = useState(null);
-    const [username, setUsername] = useState(null);
-    const [user,setUser]=useState(null);
-    const [draw,setDraw]=useState(0);
-    const [win,setWin]=useState(0);
-    const [lose,setLose]=useState(0);
-    const [totalGame,setTotalGame]=useState(0);
-  
-  
-    const [series,setSeries]=useState([]);
-    const [options,setOptions]=useState({
-      
-      labels:['Win','Lose','Draw'],
-      colors: ['#00FF00', '#FF0000', '#3F51B5'],
-      plotOptions: {
-        pie: {
-          donut: {
-            labels: {
-              show: true,
-              name: {
-                color:'black'
-              },
-              value: {
-                color:'black'
-              },
-            }
-          }
+const Profile = ({ turnOnLoading, turnOffLoading }) => {
+  // Styles
+  const classes = useStyles();
+
+  // States
+  const [user, setUser] = useState(null);
+  const [draw, setDraw] = useState(0);
+  const [win, setWin] = useState(0);
+  const [lose, setLose] = useState(0);
+  const [totalGame, setTotalGame] = useState(0);
+
+  const [series, setSeries] = useState([]);
+  const [options] = useState({
+    labels: ["Win", "Lose", "Draw"],
+    colors: ["#00FF00", "#FF0000", "#3F51B5"],
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            name: {
+              color: "black",
+            },
+            value: {
+              color: "black",
+            },
+          },
+        },
+      },
+    },
+  });
+  useEffect(() => {
+    if (user === null) {
+      turnOnLoading();
+      const getUser = async () => {
+        const { success, data } = await apiService.getUser();
+        if (success === true && data !== user) {
+          setUser(data);
+          setWin(data.totalGameWin);
+          setLose(data.totalGameLose);
+          setTotalGame(data.totalGame);
+          setDraw(data.totalGame - data.totalGameLose - data.totalGameWin);
         }
-      }
-      
-    });
-    useEffect(() => {
-    //   if (localStorage.getItem("token")) {
-    //     history.push("/");
-    //   }
-        
-        // if(user===null)
-        // {
-        //   const getUser=async()=>{
-        //     const {success,message,data}=await apiService.getUser();
-        //     if(success===true)
-        //     {
-        //       setUser(data); 
-        //       console.log("This is user after get in profile ", data);
-        //       setWin(data.totalGameWin);
-        //       setLose(data.totalGameLose);
-        //       setTotalGame(data.totalGame);
-        //       setDraw(data.totalGame-data.totalGameLose-data.totalGameWin);
+        turnOffLoading();
+      };
+      getUser();
+    }
 
-              
-        //     }
-        //   }
-        //   getUser();
-            
-        // }
-        if(user===null)
-        {
-          const getUser=async()=>{
-            const {success,message,data}=await apiService.getUser();
-            if(success===true && data!==user)
-            {
-              setUser(data); 
-              setWin(data.totalGameWin);
-              setLose(data.totalGameLose);
-              setTotalGame(data.totalGame);
-              setDraw(data.totalGame-data.totalGameLose-data.totalGameWin);
-  
-              
-            }
-          }
-          getUser();
-        }
-        
-        if(user!==null)
-        {
-            
-              //set for chart
-              let temp=[];
-              
+    if (user !== null) {
+      //set for chart
+      let temp = [];
 
-              temp.push(user.totalGameWin);
+      temp.push(user.totalGameWin);
+      temp.push(user.totalGameLose);
+      temp.push(user.totalGame - user.totalGameWin - user.totalGameLose);
 
-              temp.push(user.totalGameLose);
-              temp.push(user.totalGame-user.totalGameWin-user.totalGameLose);
+      setSeries(temp);
+      console.log("Check series:", series);
+    }
+  }, [user, series]);
 
-              setSeries(temp);
-              console.log("Check series:",series);
-        }
-        
-    }, [user,series]);
-  
-   
-
-    return (
-      <div
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
         style={{
-          justifyContent:'center',
-          alignItems:'center',
-          display:'flex',
-          flexDirection:'column',
+          border: "1px solid white",
+          alignSelf: "center",
+          justifyContent: "center",
+          backgroundColor: "rgba(255,255,255,0.9)",
         }}
       >
-        <Container
-          component="main"
-          maxWidth="xs"
-          style={{
-            border: "1px solid white",
-            alignSelf: "center",
-            justifyContent: "center",
-            backgroundColor: "rgba(255,255,255,0.9)",
-            
-          }}
-        >
-          <CssBaseline />
-          <div className={classes.paper}>
-            
-            <h1>Profile</h1>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <h1>Profile</h1>
 
-            <form
-              className={classes.form}
-              noValidate
-            >
-              {user !==null ?
+          <form className={classes.form} noValidate>
+            {user !== null ? (
               <Grid container spacing={3}>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Username:
-                  </Typography>
+                  <Typography variant="h6">Username:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {user.username}
-                  </Typography>
-                
+                  <Typography variant="h6">{user.username}</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Email:
-                  </Typography>
+                  <Typography variant="h6">Email:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {user.email}
-                  </Typography>
-               
+                  <Typography variant="h6">{user.email}</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Total game:
-                  </Typography>
+                  <Typography variant="h6">Total game:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {totalGame}
-                  </Typography>
-               
+                  <Typography variant="h6">{totalGame}</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Win:
-                  </Typography>
+                  <Typography variant="h6">Win:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {win}
-                  </Typography>
-               
+                  <Typography variant="h6">{win}</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Lose:
-                  </Typography>
+                  <Typography variant="h6">Lose:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {lose}
-                  </Typography>
-               
+                  <Typography variant="h6">{lose}</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                  <Typography
-                    variant="h6"
-                    >
-                    Draw:
-                  </Typography>
+                  <Typography variant="h6">Draw:</Typography>
                 </Grid>
                 <Grid item xs={7}>
-                <Typography
-                    variant="h6"
-                    >
-                    {draw}
-                  </Typography>
-                
+                  <Typography variant="h6">{draw}</Typography>
                 </Grid>
-                {series.length>0?
-                <div>
-                  
-                  <Grid item xs={12}>
-                      <Chart options={options} series={series} type="donut" width="330px" />
-
-                  </Grid>
-                </div>
-                
-                :
-                <div></div>
-              }         
+                {series.length > 0 ? (
+                  <div>
+                    <Grid item xs={12}>
+                      <Chart
+                        options={options}
+                        series={series}
+                        type="donut"
+                        width="330px"
+                      />
+                    </Grid>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
               </Grid>
-              :<div></div>
-              }
-                    
-                  
-               
-      
-              
-            </form>
-          </div>
-        </Container>
-        
-        
-      </div>
-    );
+            ) : (
+              <div></div>
+            )}
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
 };
-  
-  
-  
-export default Profile;
-  
+
+const mapDispatchToProps = (dispatch) => ({
+  turnOnLoading: () => {
+    dispatch(action.LOADING.turnOn());
+  },
+
+  turnOffLoading: () => {
+    dispatch(action.LOADING.turnOff());
+  },
+});
+
+export default connect(() => ({}), mapDispatchToProps)(Profile);
